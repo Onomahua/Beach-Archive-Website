c66178736328cc21452b9936b169aa5d0a5586bc
Onomahua <412228919@qq.com>
2026-05-20 23:16:02 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -15,8 +15,8 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 /* Initialize client only if credentials look real */
 if (
-  SUPABASE_URL !== 'YOUR_SUPABASE_URL' &&
-  SUPABASE_ANON_KEY !== 'YOUR_SUPABASE_ANON_KEY' &&
+const SUPABASE_URL      = 'https://lvjbxhjmsfvljldbigzq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2amJ4aGptc2Z2bGpsZGJpZ3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNjc4NDEsImV4cCI6MjA5NDg0Mzg0MX0.Lirto0HikhKa_pnQkcvJp0rMjAxccWNuY5X4Dk3sKaE';
   typeof supabase !== 'undefined'
 ) {
   window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
